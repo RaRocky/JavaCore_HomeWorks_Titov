c0eca136169eaf506d8d30f32d8c5017e1596b33
Dmitry_Titov <cntitov@gmail.com>
2022-08-11 16:08:44 +0300

    Lesson2_HomeWork2

---
+++ src/ru/geekbrains/java_core/lesson2/Home_Work/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_core.lesson2.Home_Work;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/ru/geekbrains/java_core/lesson2/Home_Work/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_core.lesson2.Home_Work;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
